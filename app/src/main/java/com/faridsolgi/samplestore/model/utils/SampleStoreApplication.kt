dbf2e95fabc21b97618aa17064f9111ce1d82b2d
faridsolgi <solgifarid@gmail.com>
2023-04-25 19:03:52 +0330

    init SampleStoreApplication.kt

---
+++ app/src/main/java/com/faridsolgi/samplestore/model/utils/SampleStoreApplication.kt
@@ -0,0 +1,7 @@
+package com.faridsolgi.samplestore.model.utils
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SampleStoreApplication :Application()
