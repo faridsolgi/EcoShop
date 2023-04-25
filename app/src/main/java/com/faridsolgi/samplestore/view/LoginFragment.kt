fd8934d852fa627a60a19b939d6fbba05cd78ca4
faridsolgi <solgifarid@gmail.com>
2023-04-25 19:03:39 +0330

    init LoginFragment.kt

---
+++ app/src/main/java/com/faridsolgi/samplestore/view/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.faridsolgi.samplestore.view
+
+import com.faridsolgi.samplestore.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+    override fun fragmentBody() {
+
+    }
+}
