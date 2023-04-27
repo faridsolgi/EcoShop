4c66d47c6caaaeb7fc478b133121e9c734563dce
faridsolgi <solgifarid@gmail.com>
2023-04-27 21:31:36 +0330

    add view binding to MainActivity.kt and transaction to navigation component

--- app/src/main/java/com/faridsolgi/ecoshop/MainActivity.kt
+++ app/src/main/java/com/faridsolgi/ecoshop/MainActivity.kt
@@ -2,10 +2,26 @@ package com.faridsolgi.ecoshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.faridsolgi.ecoshop.databinding.ActivityMainBinding
+import com.faridsolgi.ecoshop.model.utils.navHost
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    var _binding: ActivityMainBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+    override fun onNavigateUp(): Boolean {
+        return navHost(binding.root).navController.navigateUp() || super.onNavigateUp()
     }
 }
