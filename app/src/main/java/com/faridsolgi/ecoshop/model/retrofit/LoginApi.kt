fbcd506c052cf733d3eafd667ac3845a064c12d8
faridsolgi <solgifarid@gmail.com>
2023-04-27 21:28:49 +0330

    change init LoginApi.kt authUser Body

--- app/src/main/java/com/faridsolgi/ecoshop/model/retrofit/LoginApi.kt
+++ app/src/main/java/com/faridsolgi/ecoshop/model/retrofit/LoginApi.kt
@@ -1,10 +1,12 @@
 package com.faridsolgi.ecoshop.model.retrofit
 
+import com.faridsolgi.ecoshop.model.LoginRequest
 import com.faridsolgi.ecoshop.model.LoginResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface LoginApi {
     @POST("/auth/login")
-    suspend fun authUser(@Body username:String , @Body password:String) : LoginResponse?
+    suspend fun authUser(@Body loginRequest: LoginRequest) : Response<LoginResponse?>
 }
