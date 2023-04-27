e6e08e4da8b2b95a67676094e72f195fe52c2e93
faridsolgi <solgifarid@gmail.com>
2023-04-27 21:28:19 +0330

    add fragment_home.xml & HomeFragment.kt

---
+++ app/src/main/java/com/faridsolgi/ecoshop/view/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.faridsolgi.ecoshop.view
+
+import com.faridsolgi.ecoshop.databinding.FragmentHomeBinding
+
+class HomeFragment :BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun fragmentBody() {
+
+
+    }
+}
