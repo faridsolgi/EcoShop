d8e304f14351c38fbec499997db2ef1956510351
faridsolgi <solgifarid@gmail.com>
2023-04-27 21:29:03 +0330

    init init LoginRepository.kt

---
+++ app/src/main/java/com/faridsolgi/ecoshop/model/LoginRequest.kt
@@ -0,0 +1,10 @@
+package com.faridsolgi.ecoshop.model
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(
+    @SerializedName("username")
+    var username :String    ,
+    @SerializedName("password")
+    var password :String
+)
