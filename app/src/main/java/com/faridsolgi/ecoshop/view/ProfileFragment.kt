b0744c92c67540e21125ac1d32bb119da56f0218
faridsolgi <solgifarid@gmail.com>
2023-04-28 19:37:41 +0330

    init profile

---
+++ app/src/main/java/com/faridsolgi/ecoshop/view/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.faridsolgi.ecoshop.view
+
+import com.faridsolgi.ecoshop.databinding.FragmentProfileBinding
+
+class ProfileFragment :BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+    override fun fragmentBody() {
+
+    }
+}
