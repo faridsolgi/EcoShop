a1598b6e26de669a2daf7384021263cfe77cc45d
faridsolgi <solgifarid@gmail.com>
2023-05-01 22:50:37 +0330

    add some Constants to Constants.kt

--- app/src/main/java/com/faridsolgi/ecoshop/model/utils/Constants.kt
+++ app/src/main/java/com/faridsolgi/ecoshop/model/utils/Constants.kt
@@ -2,3 +2,7 @@ package com.faridsolgi.ecoshop.model.utils
 
 const val BASE_URL = "https://fakestoreapi.com/"
 const val CONNECTION_TIME = 60L
+const val PRICE_REGEX="^\\d{0,8}(\\.\\d{1,4})?\$"
+const val URL_REGEX="https?:\\/\\/(?:www\\.)?([-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b)*(\\/[\\/\\d\\w\\.-]*)*(?:[\\?])*(.+)*"
+const val ROOM_DB_NAME = "ecoShopDB"
+
